cafb232da2b25f54e26bd78a5d2a5640763335d6
NgazizahPujiPertiwi <44403533+NgazizahPujiPertiwi@users.noreply.github.com>
2018-10-24 15:38:23 +0700

    Add files via upload

---
+++ PBO Teori/src/PBO_TUGAS.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class PBO_TUGAS {
+   
+    public static void main(String[] args) {
+        int pil;
+        Scanner s = new Scanner (System.in);
+        do{
+            System.out.println("1. Input Data");
+            System.out.println("2. Tampil Data");
+            System.out.println("3. Keluar");
+            System.out.println("Pilih : ");
+            pil=s.nextInt();
+        }
+        while(pil!=3);
+        
+    }
+    }
+
+
---
+++ PBO Teori/src/program.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class program {
+   
+    public static void main(String[] args) {
+        int pil;
+        Scanner s = new Scanner (System.in);
+        do{
+            System.out.println("1. Input Data");
+            System.out.println("2. Tampil Data");
+            System.out.println("3. Keluar");
+            System.out.println("Pilih : ");
+            pil=s.nextInt();
+        }
+        while(pil!=3);
+        
+    }
+    }
+
+
